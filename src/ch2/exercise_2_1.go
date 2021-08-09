0f9b2e537d5c6f301651b3bfe9d920a3a0eaa5e3
Kai Ling Tang <ktang62@gatech.edu>
2021-08-09 18:21:12 +0800

    move files to src folder

---
+++ src/ch2/exercise_2_1.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"ch2/tempconv"
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Brrr! %v\n", tempconv.AbsoluteZeroC)
+}
---
+++ src/ch2/tempconv/conv.go
@@ -0,0 +1,27 @@
+package tempconv
+
+// Exercise 2.1
+
+func CToF(c Celsius) Farenheit {
+	return Farenheit(c*9/5 + 32)
+}
+
+func CToK(c Celsius) Kelvin {
+	return Kelvin(c - 273.15)
+}
+
+func FToC(f Farenheit) Celsius {
+	return Celsius((f - 32) * 5 / 9)
+}
+
+func FToK(f Farenheit) Kelvin {
+	return CToK(FToC(f))
+}
+
+func KToC(k Kelvin) Celsius {
+	return Celsius(k + 273.15)
+}
+
+func KToF(k Kelvin) Farenheit {
+	return CToF(KToC(k))
+}
